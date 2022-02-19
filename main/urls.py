70c1a1926ef1ab9343a9175a86878c8c3f85054f
ZhukAV <A.V.Zhuk@outlook.com>
2022-02-19 04:13:39 +0200

    prepare to deploy

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.loginPage, name = 'loginPage'),
     path('register', views.registerPage, name = 'registerPage'),
     path('logout',views.logout, name = "logout"),
     path('main',views.mainPage, name = 'mainPage'),
-    path('edit',views.edit, name = 'edit')
+    path('edit',views.edit, name = 'edit'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
